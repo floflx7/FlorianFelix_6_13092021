24e8814fd7d1f2e90d071ed8f5a65bc0c240b56d
floflx7 <82363226+floflx7@users.noreply.github.com>
2021-09-27 14:00:47 +0200

    refresh commit

--- backend/dbConfig.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(
-    "mongodb+srv://floflx:Lauflo.126@cluster0.tkpnv.mongodb.net/api-post?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => console.log("Connexion à MongoDB réussie !"))
-  .catch(() => console.log("Connexion à MongoDB échouée !"));
